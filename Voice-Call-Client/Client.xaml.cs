a538000dd03aac857d115806c3d121f79e03fdab
민기 김 <mgk9718@gmail.com>
2019-07-24 17:17:37 +0900

    TCP 완료

---
+++ Voice-Call-Client/Client.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio.Wave;
+namespace Voice_Call_Client
+{
+    public partial class MainWindow : Window
+    {
+        const string IP = "127.0.0.1";
+        const int PORT = 4321;
+
+        WaveIn waveSource = null;
+        
+
+        TcpClient tcpClient = null;
+        NetworkStream stream = null;
+        List<byte> vs = new List<byte>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            waveSource = new WaveIn();
+            waveSource.WaveFormat = new WaveFormat(48000, 2);
+            waveSource.RecordingStopped += WaveSource_RecordingStopped;
+            waveSource.DataAvailable += WaveSource_DataAvailable;
+            waveSource.StartRecording();
+
+            tcpClient = new TcpClient();
+            tcpClient.Connect(IP, PORT);
+
+            stream = tcpClient.GetStream();
+
+
+
+        }
+
+        private void ClientBackgroundWokrer_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+        }
+
+        private void WaveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (waveSource != null)
+            {
+                waveSource.Dispose();
+                waveSource = null;
+            }
+        }
+
+        private void WaveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            int len = e.BytesRecorded;
+            stream.WriteByte(  (byte)(len / 256 / 256)  );
+            stream.WriteByte(  (byte)(len / 256)  );
+            stream.WriteByte(  (byte)(len % 256)  );
+
+            stream.Write(e.Buffer, 0, e.BytesRecorded);
+        }
+    }
+}
---
+++ Voice-Call-Server/Server.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio.Wave;
+
+namespace Voice_Call_Server
+{
+    public partial class MainWindow : Window
+    {
+        const int PORT = 4321;
+        TcpListener listener = null;
+        BackgroundWorker serverBackgroundWorker = null;
+        WaveOut waveOut = null;
+
+        int index = 0;
+
+        byte[] firstBytes;
+        byte[] secondBytes;
+        public MainWindow()
+        {
+            InitializeComponent();
+            serverBackgroundWorker = new BackgroundWorker();
+            serverBackgroundWorker.DoWork += ServerBackgroundWorker_DoWork;
+            serverBackgroundWorker.RunWorkerAsync();
+
+            waveOut = new WaveOut();
+        }
+
+        private void ServerBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            listener = new TcpListener(IPAddress.Any, PORT);
+            listener.Start();
+
+            TcpClient tcp = listener.AcceptTcpClient();
+            NetworkStream stream = tcp.GetStream();
+            while (true)
+            {
+                int len = 0;
+                len += stream.ReadByte() * 256 * 256;
+                len += stream.ReadByte() * 256;
+                len += stream.ReadByte();
+
+                IWaveProvider provider;
+                if (index == 0)
+                {
+                    firstBytes = new byte[len];
+                    stream.Read(firstBytes, 0, len);
+                    if(secondBytes == null)
+                    {
+                        index = 1;
+                        continue;
+                    }
+                    provider = new RawSourceWaveStream(
+                         new MemoryStream(secondBytes), new WaveFormat()
+                    );
+                    index = 1;
+                }
+                else
+                {
+                    secondBytes = new byte[len];
+                    stream.Read(secondBytes, 0, len);
+                    provider = new RawSourceWaveStream(
+                         new MemoryStream(firstBytes), new WaveFormat()
+                    );
+                    index = 0;
+                }
+                waveOut.Init(provider);
+                waveOut.Play();
+            }
+        }
+    }
+}
